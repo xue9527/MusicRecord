d83fec06327c7ca358c007361d9bc2db9ae8ce7c
c8610290927 <c8610290927@gmail.com>
2020-11-18 18:19:11 +0800

    Initial Commit

---
+++ MusicRecord/MusicRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicRecordApp.swift
+//  MusicRecord
+//
+//  Created by User20 on 2020/11/18.
+//
+
+import SwiftUI
+
+@main
+struct MusicRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
